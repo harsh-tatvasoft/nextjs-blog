ca108d447ed082c431c277c823825ffc006edabe
Harsh Patel <harsh.tatvasoft@gmail.com>
2021-05-10 17:40:41 +0530

    Basic tweaks

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React from 'react';
 
@@ -5,7 +7,23 @@ const individualPost = () => {
     const router = useRouter();
     const { id } = router.query;
 
-    return <div>Post: {id}</div>;
+    return (
+        <div>
+            <Head>
+                <title>Harsh Patel | Post</title>
+            </Head>
+
+            <div>
+                <div>Post: {id}</div>
+
+                <div>
+                    <Link href="/">
+                        <a>Go back to Home</a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default individualPost;
